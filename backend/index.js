3b2a707fff198103d7cc71e29ea34b125113a31d
prakash-gph <gphprakash9@gmail.com>
2025-07-14 11:52:18 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express, { json } from "express"
-import cors from "cors"
 import dotenv from "dotenv";
-import { volunteerRouters } from "./router/urouter.js";
 import connectdb from "./database/db.js"
+/*
+import cors from "cors"
+
+import { volunteerRouters } from "./router/urouter.js";
+
 import { adminLogin } from "./adminControllers/adminAuthController.js";
 import connectedCloudinary from "./nodemailer/cloudinary.js";
 import { imageUpload, imageGet, imageDelete, videosUpload, videoGet, videoDelete } from "./adminControllers/adminImagesUploadControllers.js";
@@ -11,7 +14,7 @@ import multer from "multer";
 // import { uploadImages,uploadVideos } from "./adminControllers/imagesVideosControllers.js";
 // import uploadImages, { getImage } from "./adminControllers/adminImagesUploadControllers.js";
 // import { imaSave } from "./adminControllers/adminImagesUploadControllers.js";
-
+*/
 const port = process.env.PORT || 3000;
 
 dotenv.config()
@@ -22,16 +25,16 @@ console.log(process.env.PORT)
 
 connectdb()
    
-    app.use(json())
-    app.use(cors())
-    app.use(volunteerRouters)
-    app.use(adminLogin)
-    app.use(imageUpload);
-    app.use(imageGet);
-    app.use(imageDelete,);
-    app.use(videosUpload);
-    app.use(videoGet);
-    app.use(videoDelete);
+    // app.use(json())
+    // app.use(cors())
+    // app.use(volunteerRouters)
+    // app.use(adminLogin)
+    // app.use(imageUpload);
+    // app.use(imageGet);
+    // app.use(imageDelete,);
+    // app.use(videosUpload);
+    // app.use(videoGet);
+    // app.use(videoDelete);
 
     // app.use(adminSendOtp)
     // app.use(resetPassword)
@@ -41,46 +44,45 @@ connectdb()
     // app.use(getImage)
     // app.use(otpVerifiy)
 
-    connectedCloudinary()
-    app.use((err, req, res, next) => {
+    // connectedCloudinary()
+    // app.use((err, req, res, next) => {
 
-        if (err instanceof multer.MulterError) {
+    //     if (err instanceof multer.MulterError) {
 
-            switch (err.code) {
+    //         switch (err.code) {
 
-                case "LIMIT_FILE_SIZE":
+    //             case "LIMIT_FILE_SIZE":
 
-                    if (err.field === 'video') return res.json({ success: false, message: "File is large : Maximum video size 30 MB" })
-
-
-
-                    return res.json({ success: false, message: "File is large : Maximum image size 5 MB " })
-
-                default:
-
-                    if (err.field === 'video') return res.json({ success: false, message: " Please single file upload" })
+    //                 if (err.field === 'video') return res.json({ success: false, message: "File is large : Maximum video size 30 MB" })
 
 
 
-                    if (err.field === 'images') {
-                        console.log(err)
-                        return res.json({ success: false, message: "Maximum 5 images selecte" })
+    //                 return res.json({ success: false, message: "File is large : Maximum image size 5 MB " })
 
-                    }
+    //             default:
 
-            }
-        }
-        else {
-            return res.json({ success: false, message: err.message })
-        }
+    //                 if (err.field === 'video') return res.json({ success: false, message: " Please single file upload" })
 
-    })
+
+
+    //                 if (err.field === 'images') {
+    //                     console.log(err)
+    //                     return res.json({ success: false, message: "Maximum 5 images selecte" })
+
+    //                 }
+
+    //         }
+    //     }
+    //     else {
+    //         return res.json({ success: false, message: err.message })
+    //     }
+
+    // })
 
 
  app.get("/", (req, res) => {
         res.json("Api is Running")
     })
-
     app.listen(port, () => {
         console.log(`Server is running http://localhost:${port}`)
     })
