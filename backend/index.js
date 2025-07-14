f554bc5ac68be1b0e2912ed11bbd20a2cb310099
prakash-gph <gphprakash9@gmail.com>
2025-07-14 14:03:11 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import express, { json } from "express"
 import dotenv from "dotenv";
-// import connectdb from "./database/db.js"
- import cors from "cors"
+import connectdb from "./database/db.js"
+import cors from "cors"
 
-//import { volunteerRouters } from "./router/urouter.js";
-// import { adminLogin } from "./adminControllers/adminAuthController.js";
+import { volunteerRouters } from "./router/urouter.js";
+import { adminLogin } from "./adminControllers/adminAuthController.js";
+
 // import connectedCloudinary from "./nodemailer/cloudinary.js";
 
 // import { imageUpload, imageGet, imageDelete, videosUpload, videoGet, videoDelete } from "./adminControllers/adminImagesUploadControllers.js";
@@ -17,12 +18,12 @@ dotenv.config()
 
 const app = express()
 
-// connectdb()
+connectdb()
 
-// app.use(json())
- app.use(cors())
-// app.use(volunteerRouters)
-//app.use(adminLogin)
+app.use(json())
+app.use(cors())
+app.use(volunteerRouters)
+app.use(adminLogin)
 // app.use(imageUpload);
 // app.use(imageGet);
 // app.use(imageDelete,);
